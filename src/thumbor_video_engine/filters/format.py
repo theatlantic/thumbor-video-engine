2b4595b6ed85800f41b66ee4218814dd0ff00732
Frankie Dintino <fdintino@gmail.com>
2024-07-08 13:28:09 -0400

    fix: use same base ALLOWED_FORMATS list as thumbor (#25)

--- src/thumbor_video_engine/filters/format.py
+++ src/thumbor_video_engine/filters/format.py
@@ -1,11 +1,18 @@
 from thumbor.filters import BaseFilter, filter_method, PHASE_PRE_LOAD
+import thumbor.filters.format
 from thumbor.utils import logger
 
 from thumbor_video_engine.compat import filter_retval
 
 
-ALLOWED_FORMATS = [
-    'png', 'jpeg', 'jpg', 'gif', 'webp', 'webm', 'mp4', 'hevc', 'h264', 'h265', 'vp9']
+ALLOWED_FORMATS = list(thumbor.filters.format.ALLOWED_FORMATS) + [
+    "webm",
+    "mp4",
+    "hevc",
+    "h264",
+    "h265",
+    "vp9",
+]
 
 
 class Filter(BaseFilter):
@@ -13,12 +20,12 @@ class Filter(BaseFilter):
 
     @filter_method(BaseFilter.String)
     def format(self, format):
-        logger.debug('Setting format to %s' % format)
+        logger.debug("Setting format to %s" % format)
         if format.lower() not in ALLOWED_FORMATS:
-            logger.warning('Format not allowed: %s' % format.lower())
+            logger.warning("Format not allowed: %s" % format.lower())
             self.context.request.format = None
         else:
-            logger.debug('Format specified: %s' % format.lower())
+            logger.debug("Format specified: %s" % format.lower())
             self.context.request.format = format.lower()
 
         return filter_retval()
